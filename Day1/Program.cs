64843d225e42f852c74fe8e53eebd5735cb34fec
Cegoco11 <cegoco@live.com>
2018-12-12 22:45:33 +0100

    Primer commit

---
+++ Day1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Day1 {
+    class Program {
+        static void Main(string[] args) {
+            //FirstPart();
+            SecondPart();
+        }
+
+        static void FirstPart() {
+            string[] lines = System.IO.File.ReadAllLines("..\\..\\datos.txt");
+
+            int total = 0;
+
+            foreach (string line in lines) {
+                total += int.Parse(line);
+            }
+
+            Console.WriteLine("Total: " + total);
+
+            Console.ReadKey();
+        }
+
+        static void SecondPart() {
+            string[] lines = System.IO.File.ReadAllLines("..\\..\\datos.txt");
+            List<int> linesInt = lines.Select(x => int.Parse(x)).ToList();
+
+            int aux = 0;
+            List<int> resultados = new List<int>();
+
+            bool condition = true;
+
+            DateTime dt = DateTime.Now;
+
+            while (condition) {
+
+                foreach (int line in linesInt) {
+                    aux += line;
+
+                    if (resultados.Contains(aux)) {
+                        condition = false;
+                    } else {
+                        resultados.Add(aux);
+                    }
+
+                }
+
+            }
+
+            DateTime dt2 = DateTime.Now;
+            Console.WriteLine(dt2.Subtract(dt).TotalSeconds);
+
+            Console.WriteLine("Frequency  " + aux);
+
+            Console.ReadKey();
+
+        }
+
+    }
+}
---
+++ Day2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2 {
+    class Program {
+        static void Main(string[] args) {
+            FirstPart();
+        }
+
+        static void FirstPart() {
+
+            int total_cont2 = 0;
+            int total_cont3 = 0;
+
+            int cont2 = 0;
+            int cont3 = 0;
+            int aux = 0;
+
+            string[] lines = System.IO.File.ReadAllLines("..\\..\\datos.txt");
+            for (int a = 0; a < lines.Count(); a++) {
+                string line = lines[a];
+
+                cont2 = 0;
+                cont3 = 0;
+                
+                for (int i = 0; i < line.Count(); i++) {
+                    
+                    aux = 0;
+
+                    for (int j = 0; j < line.Count(); j++) {
+
+                        if (line[i] == line[j]) {
+                            aux++;
+                        }
+                    }
+
+                    if (aux == 2) {
+                        cont2++;
+                    } else if (aux == 3) {
+                        cont3++;
+                    }
+
+                }
+
+                if (cont2>=1) {
+                    total_cont2++;
+                }
+
+                if (cont3 >= 1) {
+                    total_cont3++;
+                }
+
+            }
+
+            Console.WriteLine("CheckSum " + total_cont2 * total_cont3);
+
+            Console.ReadKey();
+        }
+
+        static void SecondPart() {
+            // TODO
+        }
+    }
+}
